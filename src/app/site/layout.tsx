5dc582fce406783dcc5a91535bbc3e9d5a970877
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-05-24 02:41:48 +0430

    Just added landing page

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,9 +1,13 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import Navigation from "@/components/site/navigation";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="h-full">{children}</main>;
+  return (
+    <main className="h-full">
+      <Navigation />
+      {children}
+    </main>
+  );
 };
 
 export default layout;
