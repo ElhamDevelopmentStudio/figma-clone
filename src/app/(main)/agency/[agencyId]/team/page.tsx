2f5dca035a85e28fbe3a64a5d1c8dd164b8634b2
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-06-18 02:44:56 +0430

    created the team page and configured the send invitation feature for the sub accounts

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,6 +1,10 @@
 import { db } from "@/lib/db";
 import React from "react";
 import DataTable from "./DataTable";
+import { PlusIcon } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
+import { columns } from "./Columns";
+import SendInvitation from "@/components/forms/SendInvitation";
 
 type Props = {
   params: {
@@ -9,7 +13,8 @@ type Props = {
 };
 
 const BillingPage = async ({ params }: Props) => {
-  const authUser = await db.user.findMany({
+  const authUser = await currentUser();
+  const teamMembers = await db.user.findMany({
     where: {
       id: params.agencyId,
     },
@@ -19,7 +24,7 @@ const BillingPage = async ({ params }: Props) => {
     },
   });
 
-  if (!authUser) return null;
+  if (!teamMembers) return null;
 
   const agencyDetails = await db.agency.findUnique({
     where: { id: params.agencyId },
@@ -28,7 +33,20 @@ const BillingPage = async ({ params }: Props) => {
 
   if (!agencyDetails) return;
 
-  return <DataTable></DataTable>;
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <PlusIcon size={15} />
+          Add
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue="name"
+      columns={columns}
+      data={teamMembers}
+    ></DataTable>
+  );
 };
 
 export default BillingPage;
